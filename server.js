d4ae49b89ea2a971cfdb9d8c9884b267dead989d
jamesbieber <jamesbieber97@gmail.com>
2019-05-18 12:51:18 -0700

    Updated '/' endpoint

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ server.get("/", (req, res) => {
   res.send(`
     <h2>Welcome to the <strong>WEBAPI Sprint Challenge by James Bieber</strong></h2>
     <ul>
-      <li><strong>get()</strong></li>
-      <li><strong>insert()</strong></li>
-      <li><strong>update()</strong></li>
-      <li><strong>remove()</strong></li>
+      <li><strong>get()</strong> Get requests can be made to both localhost3000/api/projects and localhost3000/api/actions with an ID if you wish.</li>
+      <li><strong>insert()</strong> Post requests can be made to both localhost3000/api/projects and localhost3000/api/actions.</li>
+      <li><strong>update()</strong> Updates can be made to both localhost3000/api/projects/:id and localhost3000/api/actions/ ID is required for projects, but provided in body for actions.</li>
+      <li><strong>remove()</strong> Delete requests can be made to both locahost3000/api/projects/:id and localhost3000/api/actions/:id.</li>
     </ul>
   `);
 });
